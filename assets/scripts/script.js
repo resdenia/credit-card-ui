6e660da1d25378a8684bb943a16615ce629fa7cf
AntonKolomiiets <anton.kolomiiets@logz.io>
2022-09-14 23:14:24 +0300

    update

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,9 +1,24 @@
 class CreditCardHandler {
-    constructor() {}
+    constructor(formSelector) {
+        this.form = document.querySelector(formSelector);
+        this._inputs = this.form.querySelectorAll('input');
+    }
 
-    init = () => {};
+    init = () => {
+        this.onChangeInfo();
+    };
     onSubmitHandler = () => {};
+    onChangeCardNumber = () => {};
+    onChangeInfo = () => {
+        this._inputs.forEach((input) => {
+            input.addEventListener('input', (e) => {
+                document.querySelector(
+                    `[cardInfo='${e.target.name}']`,
+                ).textContent = e.target.value;
+            });
+        });
+    };
 }
 
-const credit = new CreditCardHandler();
+const credit = new CreditCardHandler('.credit-card-form');
 credit.init();
