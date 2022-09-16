bcac0a6ead196c317edf874a90fd96b405f0b4f2
AntonKolomiiets <anton.kolomiiets@logz.io>
2022-09-16 22:03:23 +0300

    add js for card

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,10 @@
+const INPUT_TYPE = 'deleteContentBackward';
+const CARD_NUMBER_IDENTIFIER = 'cardNumber';
+
+const settings = {
+    cardNumberClass: '.card-number',
+};
+
 class CreditCardHandler {
     constructor(formSelector) {
         this.form = document.querySelector(formSelector);
@@ -14,36 +21,37 @@ class CreditCardHandler {
         for (let i = 0; i < 16; i++) {
             const spanNumber = document.createElement('span');
             spanNumber.textContent = '0';
-            document.querySelector('.card-number').appendChild(spanNumber);
+            document
+                .querySelector(settings.cardNumberClass)
+                .appendChild(spanNumber);
         }
     };
-    onSubmitHandler = () => {};
-    onChangeCardNumber = () => {};
-    seperationHandler = () => {};
+
     onChangeInfo = () => {
         this._inputs.forEach((input) => {
-            input.addEventListener('keydown', (e) => {
+            input.addEventListener('input', (e) => {
                 const { name, value } = e.target;
-
                 const arrayInputValue = value.split('');
-                if (name === 'cardNumber') {
-                    if (arrayInputValue.length === 16) {
+                if (name === CARD_NUMBER_IDENTIFIER) {
+                    if (arrayInputValue.length > 16) {
                         return;
                     }
-                    if (e.which === 8) {
-                        if (arrayInputValue.length === 0) {
-                            return;
-                        }
+                    if (e.inputType === INPUT_TYPE) {
                         this._cursor = this._cursor - 1;
                         document.querySelector(
                             `[cardInfo='${name}'] span:nth-child(${this._cursor})`,
                         ).textContent = '0';
                         return;
                     }
-                    this._cursor = this._cursor + 1;
                     document.querySelector(
                         `[cardInfo='${name}'] span:nth-child(${this._cursor})`,
-                    ).textContent = arrayInputValue[arrayInputValue.length - 1];
+                    ).textContent =
+                        arrayInputValue[
+                            arrayInputValue.length === 1
+                                ? 0
+                                : arrayInputValue.length - 1
+                        ];
+                    this._cursor = this._cursor + 1;
                 } else {
                     document.querySelector(`[cardInfo='${name}']`).textContent =
                         value;
