a6a29184361ad1167c87ad361e1e3eef7ec1f504
AntonKolomiiets <anton.kolomiiets@logz.io>
2022-09-16 16:33:28 +0300

    update page

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,32 +2,51 @@ class CreditCardHandler {
     constructor(formSelector) {
         this.form = document.querySelector(formSelector);
         this._inputs = this.form.querySelectorAll('input');
+        this._cursor = 1;
     }
 
     init = () => {
         this.onChangeInfo();
+        this.renderInitialCardNumber();
+    };
+
+    renderInitialCardNumber = () => {
+        for (let i = 0; i < 16; i++) {
+            const spanNumber = document.createElement('span');
+            spanNumber.textContent = '0';
+            document.querySelector('.card-number').appendChild(spanNumber);
+        }
     };
     onSubmitHandler = () => {};
     onChangeCardNumber = () => {};
+    seperationHandler = () => {};
     onChangeInfo = () => {
         this._inputs.forEach((input) => {
-            input.addEventListener('input', (e) => {
-                if (e.target.name === 'cardNumber') {
-                    if (e.target.value.toString().split('').length / 4 > 4) {
+            input.addEventListener('keydown', (e) => {
+                const { name, value } = e.target;
+
+                const arrayInputValue = value.split('');
+                if (name === 'cardNumber') {
+                    if (arrayInputValue.length === 16) {
                         return;
                     }
-                    const position =
-                        e.target.value.toString().split('').length / 4;
-                    console.log(Math.floor(position));
+                    if (e.which === 8) {
+                        if (arrayInputValue.length === 0) {
+                            return;
+                        }
+                        this._cursor = this._cursor - 1;
+                        document.querySelector(
+                            `[cardInfo='${name}'] span:nth-child(${this._cursor})`,
+                        ).textContent = '0';
+                        return;
+                    }
+                    this._cursor = this._cursor + 1;
                     document.querySelector(
-                        `[cardInfo='${
-                            e.target.name
-                        }'] span:nth-child(${Math.floor(position + 1)})`,
-                    ).textContent = e.target.value;
+                        `[cardInfo='${name}'] span:nth-child(${this._cursor})`,
+                    ).textContent = arrayInputValue[arrayInputValue.length - 1];
                 } else {
-                    document.querySelector(
-                        `[cardInfo='${e.target.name}']`,
-                    ).textContent = e.target.value;
+                    document.querySelector(`[cardInfo='${name}']`).textContent =
+                        value;
                 }
             });
         });
